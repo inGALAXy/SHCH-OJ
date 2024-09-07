20759e16a91c285a0111324c2f66f54ab854f16d
GALAXy <eason.wu1015@gmail.com>
2024-09-07 13:53:05 +0800

    Update a0007.cpp

--- a0007.cpp
+++ a0007.cpp
@@ -1,2 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int main(){
+    int a, b, sum = 0;
+    cin >> a >> b;
+    
+    // 確保 a 小於等於 b
+    if (a > b) {
+        swap(a, b);
+    }
+
+    // 從 a 到 b 之間每個數字的平方和
+    for (int i = a; i <= b; i++) {
+        sum += i * i;
+    }
+
+    cout << sum << '\n';
+
+    return 0;
+}
