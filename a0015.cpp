d48e6560767ff68af2ec44c6c30a6eb4c9d9d878
GALAXy <eason.wu1015@gmail.com>
2024-10-15 19:54:29 +0800

    Update a0015.cpp

--- a0015.cpp
+++ a0015.cpp
@@ -1,9 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
-    
+int main()
+{
+    int a=0,sum=0,sum1=0;
+    float num = 0;
+    cin>>a;
 
-    return 0;
+    for(int i = 1;i<=a;i++)
+    {
+        num = sqrt(i);
+        if(floor(num)==num){
+            sum+=1;
+            sum1+=i;
+        }
+    }
+    cout<<sum<<" " <<sum1;
 }
